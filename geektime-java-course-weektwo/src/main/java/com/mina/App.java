1fd67fa5cf29fa8a21a171aaf7c38d26d1242cdc
xiongxiaotong@baijiahulian.com <xiongxiaotong@baijiahulian.com>
2021-07-04 16:53:50 +0800

    [feat]commit

---
+++ geektime-java-course-weektwo/src/main/java/com/mina/App.java
@@ -0,0 +1,15 @@
+package com.mina;
+
+/**
+ * @Author: Xt
+ * @Date: 2021-07-04
+ * @Time: 16:05
+ * Copyright © BaiJiaHuLian All Rights Reserved.
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ geektime-java-course-weektwo/src/main/java/com/mina/work/HttpClientInboundHandler.java
@@ -0,0 +1,33 @@
+package com.mina.work;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * @Author: Xt
+ * @Date: 2021-07-04
+ * @Time: 16:52
+ * Copyright © BaiJiaHuLian All Rights Reserved.
+ */
+public class HttpClientInboundHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpResponse)
+        {
+            HttpResponse response = (HttpResponse) msg;
+            System.out.println("CONTENT_TYPE:" + response.headers().get(HttpHeaders.Names.CONTENT_TYPE));
+        }
+        if(msg instanceof HttpContent)
+        {
+            HttpContent content = (HttpContent)msg;
+            ByteBuf buf = content.content();
+            System.out.println(buf.toString(io.netty.util.CharsetUtil.UTF_8));
+            buf.release();
+        }
+    }
+}
